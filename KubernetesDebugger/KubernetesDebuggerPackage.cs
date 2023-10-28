b18d33bb989a6dd2c4a5e545db59c79c929fc745
Jeff Lill <jeff@lilltek.com>
2023-10-28 13:54:13 -0700

    tweaked command icon

--- KubernetesDebugger/KubernetesDebuggerPackage.cs
+++ KubernetesDebugger/KubernetesDebuggerPackage.cs
@@ -1,7 +1,26 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------------
+// FILE:	    KubernetesDebuggerPackage.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:   Copyright (c) 2021 by neonFORGE, LLC.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Runtime.InteropServices;
 using System.Threading;
+
 using Microsoft.VisualStudio.Shell;
+
 using Task = System.Threading.Tasks.Task;
 
 namespace KubernetesDebugger
@@ -25,6 +44,7 @@ namespace KubernetesDebugger
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(KubernetesDebuggerPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class KubernetesDebuggerPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +66,7 @@ namespace KubernetesDebugger
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await AttachKubernetesCommand.InitializeAsync(this);
         }
 
         #endregion
